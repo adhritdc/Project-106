34a14853dad7f210a672ea43eaa0233c8a629c68
adhritdc <82810967+adhritdc@users.noreply.github.com>
2021-12-24 15:14:09 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,1 @@
-function startClassifier() 
-{
-    navigator.mediaDevices.getUserMedia({ audio: true});
-    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/3bdVIZQqJ/model.json', modelReady);
-}
 
-function modelReady() 
-{
-    classifier.classify(gotResults);
-}
